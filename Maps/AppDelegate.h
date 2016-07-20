4d9437ba0a417614f1a43bec31532d39961c19da
Kevin Skompinski <kevons5252@gmail.com>
2016-07-19 20:51:21 -0400

    Initial Commit

---
+++ Maps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maps
+//
+//  Created by Kevin on 7/19/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Maps
+//
+//  Created by Kevin on 7/19/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
